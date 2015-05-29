b14d3d446b7c3ef30d3e5d22853ca1d908f42758
Bruce Becker <brucellino@gmail.com>
2015-05-29 18:28:04 +0200

    Added the check build script for python with the module file

---
+++ check-build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+module load ci
+echo "checking $NAME"
+cd $WORKSPACE/$NAME-$VERSION
+
+echo $?
+
+make install # DESTDIR=$SOFT_DIR
+
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+module-whatis   "$NAME $VERSION."
+setenv       PYTHON_VERSION       $VERSION
+setenv       PYTHON_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+setenv       PYTHONHOME       $::env(PYTHON_DIR)
+prepend-path PATH              $::env(PYTHON_DIR)/bin
+prepend-path LD_LIBRARY_PATH   $::env(PYTHON_DIR)/lib
+prepend-path GCC_INCLUDE_DIR   $::env(PYTHON_DIR)/include
+MODULE_FILE
+) > modules/$VERSION
+
+mkdir -p $LIBRARIES_MODULES/$NAME
+cp modules/$VERSION $LIBRARIES_MODULES/$NAME
+module add python/$VERSION
+python --version
