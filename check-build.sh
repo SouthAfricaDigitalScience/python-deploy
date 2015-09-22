1a9bcfab93d81c716b1591662c810252e08041b0
Bruce Becker <brucellino@gmail.com>
2015-09-22 13:54:49 +0200

    Trying to set the python path. I know this will not work, because we have not set the major version. Just want to trigger a build

--- check-build.sh
+++ check-build.sh
@@ -5,6 +5,7 @@ echo "checking $NAME"
 cd $WORKSPACE/Python-$VERSION
 
 
+
 echo $?
 
 make install # DESTDIR=$SOFT_DIR
@@ -23,6 +24,7 @@ module-whatis   "$NAME $VERSION."
 setenv       PYTHON_VERSION       $VERSION
 setenv       PYTHON_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
 setenv       PYTHONHOME       $::env(PYTHON_DIR)
+setenv       PYTHONPATH        $::env(PYTHON_DIR/lib/python$VERSION_MAJOR)
 prepend-path PATH              $::env(PYTHON_DIR)/bin
 prepend-path LD_LIBRARY_PATH   $::env(PYTHON_DIR)/lib
 prepend-path GCC_INCLUDE_DIR   $::env(PYTHON_DIR)/include
