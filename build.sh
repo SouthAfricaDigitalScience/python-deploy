834c889471a2287bdbcf979a7898761a098032cf
Bruce Becker <brucellino@gmail.com>
2015-12-08 20:36:36 +0100

    Added deploy.sh and updated for GCC version

--- build.sh
+++ build.sh
@@ -1,23 +1,34 @@
-#!/bin/bash
-SOURCE_FILE=$NAME-$VERSION.tar.gz
-module load ci
-echo $SOFT_DIR
-echo $WORKSPACE
-echo $SRC_DIR
-mkdir -p $SOFT_DIR $WORKSPACE $SRC_DIR
-echo $NAME
-echo $VERSION
-module load gcc/4.8.4
-if [[ ! -s $SRC_DIR/$SOURCE_FILE ]] ; then
+#!/bin/bash -e
+. /etc/profile.d/modules.sh
+SOURCE_FILE=${NAME}-${VERSION}.tar.gz
+module add ci
+module add zlib
+module add gcc/${GCC_VERSION}
+
+echo ${SOFT_DIR}
+echo ${WORKSPACE}
+echo ${SRC_DIR}
+mkdir -p ${SOFT_DIR}-gcc-${GCC_VERSION}
+mkdir -p ${WORKSPACE}
+mkdir -p ${SRC_DIR}
+module add gcc/${GCC_VERSION}
+
+if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
+  touch  ${SRC_DIR}/${SOURCE_FILE}.lock
   echo "looks like the tarball isn't there yet"
-  ls $SRC_DIR
-  mkdir -p $SRC_DIR
-  wget --no-check-certificate https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz -O $SRC_DIR/$SOURCE_FILE
+  wget --no-check-certificate https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz -O ${SRC_DIR}/${SOURCE_FILE}
+  echo "releasing lock"
+  rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
+elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
+  # Someone else has the file, wait till it's released
+  while [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; do
+    echo " There seems to be a download currently under way, will check again in 5 sec"
+    sleep 5
+  done
 fi
-tar -xzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
+tar -xz --keep-newer-files -f ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE}
 # echo $NAME | tr '[:upper:]' '[:lower:]'
-ls $WORKSPACE
 # Again with the frikkin naming conventions
-cd $WORKSPACE/Python-$VERSION
-./configure --prefix=$SOFT_DIR --enable-shared
-make -j 8
+cd ${WORKSPACE}/Python-${VERSION}
+./configure --prefix=${SOFT_DIR}-gcc-${GCC_VERSION} --enable-shared
+make
--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -e
 . /etc/profile.d/modules.sh
-module load ci
+SOURCE_FILE=${NAME}-${VERSION}.tar.gz
+module add deploy
 module add zlib
-module add  gcc/${GCC_VERSION}
-echo "checking $NAME"
+module add gcc/${GCC_VERSION}
 cd ${WORKSPACE}/Python-${VERSION}
-# Python site packages are separated out by major version numbers, so we extract that to use it later
-# in the PYTHONPATH
-VERSION_MAJOR=${VERSION:0:3} # Should be 2.7 or 3.4 or similar
-make check
-echo $?
+make distclean
+./configure --prefix=${SOFT_DIR}-gcc-${GCC_VERSION} --enable-shared
+make
 
 make install
+
+VERSION_MAJOR=${VERSION:0:3} # Should be 2.7 or 3.4 or similar
+echo $?
 mkdir -p modules
 (
 cat <<MODULE_FILE
@@ -26,7 +27,7 @@ conflict python
 module add gcc/${GCC_VERSION}
 module-whatis   "$NAME $VERSION. compiled  for GCC ${GCC_VERSION}"
 setenv       PYTHON_VERSION       $VERSION
-setenv       PYTHON_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION-gcc-${GCC_VERSION}
+setenv       PYTHON_DIR           $::env(CVMFS_DIR)/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION-gcc-${GCC_VERSION}
 #setenv       PYTHONHOME        $::env(PYTHON_DIR)
 setenv       PYTHONPATH        $::env(PYTHON_DIR)/lib/python${VERSION_MAJOR}
 prepend-path PATH              $::env(PYTHON_DIR)/bin
@@ -35,9 +36,11 @@ prepend-path GCC_INCLUDE_DIR   $::env(PYTHON_DIR)/include
 MODULE_FILE
 ) > modules/$VERSION-gcc-${GCC_VERSION}
 
-mkdir -p $LIBRARIES_MODULES/$NAME
-cp modules/$VERSION-gcc-${GCC_VERSION} $LIBRARIES_MODULES/$NAME
-module add python/$VERSION-gcc-${GCC_VERSION}
+# this should probably be in $PYTHON_MODULES instead of $LIBRARY_MODULES
+
+mkdir -p ${LIBRARIES_MODULES}/${NAME}
+cp modules/${VERSION}-gcc-${GCC_VERSION} ${LIBRARIES_MODULES}/${NAME}
+module add python/${VERSION}-gcc-${GCC_VERSION}
 echo "Our python is"
 which python
 python --version
@@ -46,13 +49,10 @@ python --version
 echo "Setting up setuptools"
 cd $WORKSPACE/Python-${VERSION}
 # First, download the setuptools package and unpack it
-wget https://pypi.python.org/packages/source/s/setuptools/setuptools-18.3.2.tar.gz
-tar xfz setuptools-18.3.2.tar.gz
 python setup.py install --prefix=${PYTHON_DIR}
 
 ## time to install pip - this also has to go into the python path.
-echo "Getting pip"
-wget https://bootstrap.pypa.io/get-pip.py
+echo "Setting up pip"
 python get-pip.py --install-option=--prefix=${PYTHON_DIR}
 
 ## run some checks
